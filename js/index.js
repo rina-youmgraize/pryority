7e12272ae0ee44ea76ba85b640ebd7f7871f9083
@noamatlas <nnoam1@gmail.com>
2025-05-09 17:23:46 +0300

    Skills

--- js/index.js
+++ js/index.js
@@ -60,6 +60,8 @@ if (p) {
         tempAreasCompleteString = "Portfolio:"
     }
     document.getElementById("areasString").innerHTML = tempAreasCompleteString;
+
+    showSkills(p.skills);
 }
 
 // document.addEventListener('DOMContentLoaded', function() {
@@ -89,3 +91,92 @@ document.getElementById("registerNew").addEventListener(
         window.location.href = `register.html`;
     }
 );
+
+
+
+
+function showSkills(skillsArr){
+    if(!Array.isArray(skillsArr)) return;
+    if(skillsArr.length == 0) return;
+    let howManyRows = 0;
+    let s = `
+        <div class="card shadow border-0 rounded-4 mb-5">
+            <div class="card-body p-5">
+                <div class="mb-5">
+                    <div class="d-flex align-items-center mb-4">
+                        <div class="feature bg-primary bg-gradient-primary-to-secondary text-white rounded-3 me-3"><i class="bi bi-tools"></i></div>
+                        <h3 class="fw-bolder mb-0"><span class="text-gradient d-inline">Professional Skills</span></h3>
+                    </div>    
+    `;
+
+    if(skillsArr.length < 4){
+        howManyRows =1;   
+    }else {
+        howManyRows = Math.ceil(skillsArr.length / 3);
+    }
+    let i = 0;
+    for(let currRow = 1; currRow <= howManyRows; currRow++){
+        if(currRow < howManyRows){
+            s += `<div class="row row-cols-1 row-cols-md-3 mb-4">`;
+        }else{
+            s += `<div class="row row-cols-1 row-cols-md-3">`;  
+        }
+        
+        // if(currRow == 1){
+        //     s += `<div class="row row-cols-1 row-cols-md-3 mb-4">`;   
+        // }else{
+        //     s += `<div class="row row-cols-1 row-cols-md-3">`;
+        // }
+        i = (currRow - 1) * 3;
+        while(i < (currRow * 3)){
+            if(skillsArr[i] == undefined) break;
+            s += `<div class="col mb-4 mb-md-0"><div class="oneSkillClass d-flex align-items-center bg-light rounded-4 p-3 h-100">${skillsArr[i]}</div></div>`;
+            // if(i < ((currRow * 3) - 1)){
+            //     s += `<div class="col mb-4 mb-md-0"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">${skillsArr[i]}</div></div>`;
+            // }else{
+            //     s += `<div class="col"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">${skillsArr[i]}</div></div>`;
+            // }
+            i++;           
+        }
+
+        s += `</div>`;
+    }
+
+
+
+                                    // <div class="row row-cols-1 row-cols-md-3 mb-4">
+                                    //     <div class="col mb-4 mb-md-0"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">SEO/SEM Marketing</div></div>
+                                    //     <div class="col mb-4 mb-md-0"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">Statistical Analysis</div></div>
+                                    //     <div class="col"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">Web Development</div></div>
+                                    // </div>
+                                    // <div class="row row-cols-1 row-cols-md-3">
+                                    //     <div class="col mb-4 mb-md-0"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">Network Security</div></div>
+                                    //     <div class="col mb-4 mb-md-0"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">Adobe Software Suite</div></div>
+                                    //     <div class="col"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">User Interface Design</div></div>
+                                    // </div>
+
+
+    s += `
+                </div>
+            </div>
+        </div>
+    `;
+
+    document.getElementById("skillsSectionContainer").innerHTML = s;
+}
+
+
+
+                        // <div class="card shadow border-0 rounded-4 mb-5">
+                        //     <div class="card-body p-5">
+                        //         <div class="mb-5">
+
+                        //             <div class="d-flex align-items-center mb-4">
+                        //                 <div class="feature bg-primary bg-gradient-primary-to-secondary text-white rounded-3 me-3"><i class="bi bi-tools"></i></div>
+                        //                 <h3 class="fw-bolder mb-0"><span class="text-gradient d-inline">Professional Skills</span></h3>
+                        //             </div>
+                                    
+                                    
+                        //         </div>
+                        //     </div>
+                        // </div>
